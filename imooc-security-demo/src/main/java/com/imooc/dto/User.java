1bd7ca880db66c3069acfcaee074f1eb6b2753ad
Mel Liu <privatemiao@gmail.com>
2017-11-18 03:54:03 +0800

    :lollipop:

--- imooc-security-demo/src/main/java/com/imooc/dto/User.java
+++ imooc-security-demo/src/main/java/com/imooc/dto/User.java
@@ -2,6 +2,10 @@ package com.imooc.dto;
 
 import com.fasterxml.jackson.annotation.JsonView;
 import org.apache.commons.lang.builder.ToStringBuilder;
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.validation.constraints.Past;
+import java.util.Date;
 
 public class User {
 
@@ -12,10 +16,16 @@ public class User {
     public interface UserDetailView extends UserSimpleView {
     }
 
+    private String id;
 
+    @NotBlank(message = "用户名不能为空")
     private String username;
 
+    @NotBlank(message = "密码不能为空")
     private String password;
+
+    @Past(message = "生日必须是过去的时间")
+    private Date birthday;
 
     public User() {
     }
@@ -37,6 +47,23 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    @JsonView(UserSimpleView.class)
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
     }
 
     @Override
--- imooc-security-demo/src/main/java/com/imooc/web/controller/UserController.java
+++ imooc-security-demo/src/main/java/com/imooc/web/controller/UserController.java
@@ -3,20 +3,48 @@ package com.imooc.web.controller;
 import com.fasterxml.jackson.annotation.JsonView;
 import com.imooc.dto.User;
 import com.imooc.dto.UserQueryCondition;
+import com.sun.org.apache.xpath.internal.SourceTree;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    //    RequestBody: 解析传过来的字符串格式的JSON
+    @PostMapping
+    public User create(@Valid @RequestBody User user, BindingResult errors) {
+
+        if (errors.hasErrors()){
+            errors.getAllErrors().stream().forEach(error -> System.out.println(error.getDefaultMessage()));
+        }
+
+        System.out.println(user);
+        user.setId("1");
+        return user;
+    }
+
+    @PutMapping("/{id}")
+    public User update(@Valid @RequestBody User user, BindingResult errors) {
+
+        if (errors.hasErrors()){
+            errors.getAllErrors().stream().forEach(error -> {
+                System.out.println(String.format("%s: %s", ((FieldError) error).getField(), error.getDefaultMessage()));
+            });
+        }
+
+        System.out.println(user);
+        user.setId("1");
+        return user;
+    }
 
     @GetMapping
     @JsonView(User.UserSimpleView.class)
